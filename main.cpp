b15352875ef9a0b4cc745a67ab8f09a438565d27
jonjonrebas <jonathan.saber@hes-so.ch>
2024-10-04 14:22:19 +0200

    labo v2
    Code alligné et avec commentaires

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <cstdlib>
-#include <cmath>
+
 
 using namespace std;
 
 int main() {
 
-    cout << "Bonjour bienvenue dans ce programme qui vous donnera tous les nombre premiers dans la limite de l'intervalle que vous aurez choisi." << endl;
+    cout
+            << "Bonjour bienvenue dans ce programme qui vous donnera tous les "
+            << "nombre premiers dans la limite de l'intervalle que vous aurez choisi." << endl;
 
     int nombre_premier;
     int limite;
@@ -15,27 +17,31 @@ int main() {
 
     do {
 
-        do { //boucle pour répéter le programme tant que l'utilisateur ne donne pas N
+        do { //Boucle pour s'assurer que la limite entrée par l'utilisateur est correcte
 
             cout << "Veuillez entrer une valeur limite comprise entre 2 et 1000 : " << endl;
             cin >> limite;
+
+            // Vérification que la limite est dans l'intervalle autorisé
 
             if (limite < 2 || limite > 1000) {
                 cout << "Veuillez entrer une limite comprise 2 et 1000 " << endl;
 
 
             }
-        }   while (limite < 2 || limite > 1000);
+        } while (limite < 2 || limite > 1000);
 
         cout << "voici tous les nombres premiers jusqu'a la limite choisie : " << endl;
 
+        // Boucle pour trouver tous les nombres premiers jusqu'à la limite
+
         for (nombre_premier = 2; nombre_premier <= limite; ++nombre_premier) {
-            bool estPremier = true;
+            bool estPremier = true; //flag pour indiquer si le nombre est premier
             for (int i = 2;
                  i * i <= nombre_premier; ++i) { //diviser par tous les nombres de 2 jusqu'à sa racine carrée
                 if (nombre_premier % i == 0) {
                     estPremier = false;
-                    break;
+                    break; // Sortir de la boucle dès qu'on trouve un diviseur
                 }
             }
             if (estPremier) {
@@ -60,7 +66,8 @@ int main() {
             if (reponse != 'O' && reponse != 'o' && reponse != 'N' && reponse != 'n') {
 
             }
-        } while (reponse != 'O' && reponse != 'o' && reponse != 'N' && reponse != 'n'); // Continue de demander tant que l'entrée est incorrecte
+        } while (reponse != 'O' && reponse != 'o' && reponse != 'N' &&
+                 reponse != 'n'); // Continue de demander tant que l'entrée est incorrecte
 
     } while (reponse == 'O' || reponse == 'o'); // Répéter si l'utilisateur entre 'O' ou 'o'
 
